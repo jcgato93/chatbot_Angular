b08d4900c55d5d461050cdf14b7d8e804bcf2862
Camilo.IT <npgcamilo@gmail.com>
2018-10-17 14:01:34 -0500

    Init project

---
+++ chatbot/src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../chat.service';
+import { Observable } from "rxjs";
+import 'rxjs/operators';
+import { scan } from 'rxjs/operators';
+
+
+
+@Component({
+  selector: 'chat-dialog',
+  templateUrl: './chat-dialog.component.html',
+  styleUrls: ['./chat-dialog.component.css']
+})
+export class ChatDialogComponent implements OnInit {
+
+  messages: Observable<Message[]>;
+  formValue: string;
+
+  constructor(private chat: ChatService) { }
+
+  ngOnInit() {
+    // appends to array after each new message is added to feedSource
+    this.messages = this.chat.conversation.asObservable ().pipe (scan ((acc, val) => acc.concat (val)))
+   
+  }
+
+
+  sendMessage(){
+    this.chat.converse(this.formValue);
+    this.formValue = '';
+  }
+
+}
